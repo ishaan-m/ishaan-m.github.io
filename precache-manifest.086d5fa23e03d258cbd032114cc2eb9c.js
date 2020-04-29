6b8b15502cb3f86e853f4140f81e95969bd58e07
ishaan-m <ishaanmanudhane1@gmail.com>
2020-04-29 20:04:40 +0530

    Updates

---
+++ precache-manifest.086d5fa23e03d258cbd032114cc2eb9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fb2691c33808d63026c0207c8d2e0e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a8a28967f668111d6269",
+    "url": "/home/static/css/main.584bac79.chunk.css"
+  },
+  {
+    "revision": "31065719170bfb6fd14c",
+    "url": "/home/static/js/2.73412ab1.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/home/static/js/2.73412ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a28967f668111d6269",
+    "url": "/home/static/js/main.e7179846.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "11550d8cdebda9917fbd0e7252a1f728",
+    "url": "/home/static/media/Ishaan_Manudhane_Resume.11550d8c.pdf"
+  }
+]);
